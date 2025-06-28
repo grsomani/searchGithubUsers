ca5516ab021c1bafb60545da6da9cfc82ee62f00
Ganesh Somani <ganesh.somani@gmail.com>
2025-06-28 14:52:18 +0530

    Initial Commit

---
+++ SearchGithub/SearchGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchGithubApp.swift
+//  SearchGithub
+//
+//  Created by Ganesh on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SearchGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SearchGithubTests/SearchGithubTests.swift
@@ -0,0 +1,16 @@
+//
+//  SearchGithubTests.swift
+//  SearchGithubTests
+//
+//  Created by Ganesh on 28/06/25.
+//
+
+import Testing
+
+struct SearchGithubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
